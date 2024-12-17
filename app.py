07d86946f4c757f73bf7862e675c1d222a5ff210
Prabhat Tripathi <65029713+darkest-opson@users.noreply.github.com>
2024-12-17 14:00:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import gseapy as gp
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import main
+# Title and Description
+st.title("Gene Enrichment Analysis App")
+st.write("Enter a list of genes to perform GO and pathway enrichment analysis using GSEAPy.")
+
+# Input Section
+genes_input = st.text_area("Enter your genes (one per line):", height=200)
+try:
+    # Submit Button
+    if st.button("Submit"):
+        gene_list = [gene.strip() for gene in genes_input.split("\n") if gene.strip()]
+except:
+    st.write(" ")
+
+Result = main.GO_path(gene_list)
+gene_sets = ['KEGG_2021_Human', 'KEGG_2019_Human', 'Reactome_Pathways_2024', 
+                                                                'WikiPathway_2023_Human', 'GO_Biological_Process_2023', 
+                                                                'GO_Cellular_Component_2023', 'GO_Molecular_Function_2023',
+                                                                'BioCarta_2016', 'Panther_2016', 'DisGeNET', 'Human_Phenotype_Ontology']
+for geneset in gene_sets:
+    st.subheader(geneset)
+    result = Result.results[Result.results["Gene_set"]==geneset]
+    st.dataframe(result)
+    # Convert DataFrame to CSV
+    csv_file = result.to_csv(index=False,sep="\t").encode('utf-8')
+  
+    # Download Button
+    st.download_button(
+        label="Download Results as CSV",
+        data=csv_file,
+        file_name=f"{geneset}.txt",
+        mime="text/csv"
+    )
